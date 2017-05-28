be9717042865490b48613af64c44b249d982209b
Paul Seyfert <pseyfert@cern.ch>
2017-05-28 17:06:32 +0200

    clang formating and a bit more minimalisation

--- Kernel/LHCbMath/LHCbMath/Power.h
+++ Kernel/LHCbMath/LHCbMath/Power.h
@@ -1,62 +1,47 @@
-// ============================================================================
-#ifndef LHCBMATH_POWER_H 
+#ifndef LHCBMATH_POWER_H
 #define LHCBMATH_POWER_H 1
 // ============================================================================
 // Include files
 // ============================================================================
-// STD & STL 
+// STD & STL
 // ============================================================================
 #include <cmath>
 // ============================================================================
 /** @file
  *
- *  This file was originally taken from the LoKi project - 
+ *  This file was originally taken from the LoKi project -
  *    "C++ ToolKit  for Smart and Friendly Physics Analysis"
  *
  *  @author Vanya BELYAEV ibelyaev@physics.syr.edu
- *  @date 2001-01-23 
+ *  @date 2001-01-23
  */
-// ============================================================================
-  // ==========================================================================
-    // ========================================================================
-    // using namespace std ; ?
-    // ========================================================================
-    // ========================================================================
-    /** Simple utility for efficient "pow".
-     *  It works only for positive integer powers.
-     *
-     *  @code 
-     *   
-     *   const double result = pow ( value , 10 ) ;
-     *
-     *  @endcode 
-     *
-     *  The actual code is copied from 
-     *     std::__cmath_power bits/cmath.tcc 
-     *
-     *  @author Vanya BELYAEV Ivan.Belyaev@lapp.in2p3.fr
-     *  @date 2005-04-09 
-     */
-    template<typename TYPE>
-    inline TYPE pow ( TYPE __x , unsigned long __n )
-    {
-      //
-      TYPE __y = __n % 2 ? __x : 1;
-      //
-      while ( __n >>= 1 )
-      {
-        __x = __x * __x;
-        if ( __n % 2) { __y = __y * __x; }
-      } 
-      //
-      return __y ;
+/** Simple utility for efficient "pow".
+ *  It works only for positive integer powers.
+ *
+ *  @code
+ *
+ *   const double result = pow ( value , 10 ) ;
+ *
+ *  @endcode
+ *
+ *  The actual code is copied from
+ *     std::__cmath_power bits/cmath.tcc
+ *
+ *  @author Vanya BELYAEV Ivan.Belyaev@lapp.in2p3.fr
+ *  @date 2005-04-09
+ */
+template <typename TYPE> inline TYPE pow(TYPE __x, unsigned long __n) {
+  //
+  TYPE __y = __n % 2 ? __x : 1;
+  //
+  while (__n >>= 1) {
+    __x = __x * __x;
+    if (__n % 2) {
+      __y = __y * __x;
     }
-    // ========================================================================
-    // ========================================================================
-
-  // ==========================================================================
-// ============================================================================
-// The END 
-// ============================================================================
+  }
+  //
+  return __y;
+}
 #endif // LHCBMATH_POWER_H
 // ============================================================================
